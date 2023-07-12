15670f8cec6569ba2fe471f2a22a8415908d2f95
Vadim <vadims.krasovskij01@gmail.com>
2023-07-12 19:37:01 +0300

    add dropDown option

--- binitex_task/src/components/navbar/navbar.js
+++ binitex_task/src/components/navbar/navbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react'
 
 import DatePicker from 'react-datepicker'
-
+import { CiCircleList } from 'react-icons/ci';
 import CovidService from '../../services/covid-service'
 import CovidTable from '../covid-table/covid-table'
 
@@ -13,12 +13,24 @@ import moment from 'moment/moment'
 
 const Navbar = () => {
 
+  const options = [
+    { value: "country", label: "Country"},
+    { value: "cases", label: "Number of cases"},
+    { value: "deaths", label: "Number of deaths"},
+    { value: "totalCases", label: "Number of cases total"},
+    { value: "totalDeaths", label: "Number of deaths total"},
+    { value: "casesOnThousands", label: "Number of cases per 1000 inhabitants"},
+    { value: "deathsOnThousands", label: "Number of deaths per 1000 inhabitants"}
+  ]
+
   const covidService = new CovidService()
 
   const [data, setData] = useState([]) 
   const [countryInputValue, setCountryInputValue] = useState("")
   const [startDate, setStartDate] = useState(new Date('2019/12/01'));
   const [endDate, setEndDate] = useState(new Date('2020/08/28'));
+  const [showMenu, setShowMenu] = useState(false);
+
 
   const getData = () => {
     covidService.getCovidData().then((allData) => {
@@ -67,30 +79,67 @@ const Navbar = () => {
     filtredDataByCountry(value);
   };
 
+  // Select  filter by value DropDown 
+
+  // useEffect(() => {
+  //   const handler = () => setShowMenu(false);
+
+  //   window.addEventListener("click", handler);
+  //   return () => {
+  //     window.removeEventListener("click", handler)
+  //   };
+  // });
+
+  // const handleDropDownInputClick = (e) => {
+  //   e.stopPropagation();
+  //   setShowMenu(!showMenu)
+  // };
+
+
+
   console.log(countryInputValue)
   console.log(filtredDataByCountry)
 
   return (
     <div className="navbar-container">
       <div className="nav-component">
-        <button className='button-cover'>Table</button>
-        <button className='button-cover'>Info</button>
         <form className='country-search-form'>
           <input 
             type='text'
             placeholder='Search country'
-            className='input-cover'
+            className='country-input-cover'
             value={countryInputValue || ""}
             onChange={(e) => handleChangeCountry(e.target.value)}
           />
           {/* <button className='country-search-btn' >Search</button> */}
         </form>
-        
+        <div className="dropdown">
+          <div className="dropdown-button" >
+            Choose One
+            <span onClick={(e) => setShowMenu(!showMenu)}><CiCircleList / ></span>
+          </div>
+          <div className="dropdown-content">
+            {showMenu && 
+              options.map((option) => (
+                <div key={option.value} className="dropdown-item">
+                  {option.label}
+                </div>
+              ))
+            }
+           
+          </div>
+        </div>
+        <input 
+            type='text'
+            placeholder='Значение от'
+            className='value-input-cover'
+          />
         <input 
           type='text'
-          placeholder='Hello world'
-          className='input-cover'
+          placeholder='Значение до'
+          className='value-input-cover'
         />
+        
       </div>
       <div style={{ margin: 0}}>
         <DatePicker 
