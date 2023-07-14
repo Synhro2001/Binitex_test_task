f9615bb5bcf637c8d0ee234fb26816c25442222b
Vadim <vadims.krasovskij01@gmail.com>
2023-07-14 17:55:50 +0300

    Finish covidGraphic component

--- binitex_task/src/components/covid-graphic/covid-graphic.js
+++ binitex_task/src/components/covid-graphic/covid-graphic.js
@@ -6,11 +6,14 @@ import 'chart.js/auto';
 
 import "./covid-graphic.css"
 
+
 const CovidGraphic = ({chartData}) => {
 
   const [countryMenu, setCountryMenu] = useState(false)
   const [countryArray, setCountryArray] = useState([])
+  
   const [selectedCountry, setSelectedCountry] = useState("")
+ 
 
   const handleCountryMenuDropDown = (countries) => {
     setSelectedCountry(countries)
@@ -30,37 +33,93 @@ const CovidGraphic = ({chartData}) => {
   }, [chartData]);
   
   console.log(countryArray);
-
-
-
-
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'Sales',
-        data: [12, 19, 3, 5, 2, 3, 8],
-        borderColor: 'rgba(75,192,192,1)',
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        fill: false,
-    
-      },
-    ],
+  
+  const getPeriodData = () => {
+    return chartData
+      .filter((item) => selectedCountry ? item.countriesAndTerritories === selectedCountry : item)
+      .reduce((accumulator, countryData) => {
+        const { dateRep, cases, deaths } = countryData;
+        let index = accumulator.findIndex(obj => obj && obj.date === dateRep);
+        
+        if (index === -1) {
+          accumulator.push({ date: dateRep, cases: 0, deaths: 0 });
+          index = accumulator.length - 1;
+        }
+        
+        accumulator[index].cases = cases;
+        accumulator[index].deaths = deaths;
+  
+        return accumulator;
+      }, []);
   };
 
   const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        labels: {
+          color: '#FFFFFF', // Устанавливаем цвет текста легенды
+        },
+      },
+      title: {
+        display: true,
+        text: 'Schedule of diseases and deaths',
+        color: '#FFFFFF', // Устанавливаем цвет текста заголовка
       },
     },
+    scales: {
+      // Добавляем опции для осей, если необходимо
+      y: {
+        ticks: {
+          color: '#FFFFFF', // Устанавливаем цвет текста на оси y
+        },
+        grid: {
+          color: '', // Устанавливаем цвет сетки оси y
+        },
+      },
+      x: {
+        reverse: true,
+        ticks: {
+          color: '#FFFFFF', // Устанавливаем цвет текста на оси x
+        },
+        grid: {
+          color: '', // Устанавливаем цвет сетки оси x
+        },
+      },
+    },  animation: {
+      duration: 1000, // Длительность анимации в миллисекундах
+      easing: 'easeInOutQuart', // Функция времени для анимации (например, 'linear', 'easeInOutQuart', 'easeOutBounce', и т.д.)
+    },
   };
+
+
+  const data = {
+    labels: getPeriodData().map((item) => item.date),
+    datasets: [
+      
+        {
+          label: 'Cases',
+          data: getPeriodData().map((item) => item.cases),
+          borderColor: 'rgb(247, 255, 0)',
+          backgroundColor: 'rgba(247, 255, 0)',
+          pointRadius: 1,
+        },
+        {
+          label: 'Deaths',
+          data: getPeriodData().map((item) => item.deaths),
+          borderColor: 'rgb(255, 0, 0)',
+          backgroundColor: 'rgba(255, 0, 0)',
+          pointRadius: 1,
+      },
+   
+    ],
+};
 
   return (
     <>
     <div className="dropdown">
       <div className="dropdown-button" >
-        {selectedCountry || "Choose one option"}
+        {selectedCountry || "Choose country"}
         <span onClick={(e) => setCountryMenu(!countryMenu)}><CiCircleList/></span>
       </div>
       <div className="dropdown-content scrool-contain">
